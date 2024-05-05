47428d75771c3f5c08b69d64967c0c23e5403966
Harman Kajla <reheharman07@gmail.com>
2024-05-05 12:35:39 +0530

    slight sol

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ def prediction(image):
 
 import gradio
 
-    gui = gradio.Interface(fn=prediction, inputs="image", outputs= gradio.Textbox(label="status")
+gui = gradio.Interface(fn=prediction, inputs="image", outputs= gradio.Textbox(label="status"))
 gui.launch(share=True)
 
