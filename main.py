138b08f465efc3f11418d056044a555791cd674c
Harman Kajla <reheharman07@gmail.com>
2024-05-05 12:13:29 +0530

    Mk 1.02

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import gradio
 from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
-import gradio as gr
+
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
@@ -37,6 +38,6 @@ def prediction(image):
     confidence_score = prediction[0][index]
     return class_name[2:], confidence_score*100
 
-gui = gr.Interface(fn=prediction, inputs="image", outputs=["text", "number"])
+gui = gradio.Interface(fn=prediction, inputs="image", outputs=["text", "number"])
 gui.launch(share=True)
 
